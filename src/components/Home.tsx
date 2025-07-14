f5d57515e59574ff3af309da08e8c6c89509fd67
joaoespdev <joaokero@hotmail.com>
2025-07-14 16:29:45 -0300

    add: new button style/adjust: main page info

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
 
 const Home: React.FC = () => {
   return (
@@ -9,39 +9,65 @@ const Home: React.FC = () => {
           <p className="meuNomeE">Olá, meu nome é</p>
           <h1 className="nome">João Victor</h1>
           <p className="souTrainee">
-            Sou trainee de desenvolvimento<br /> de software na DBServer.
+            Sou trainee de desenvolvimento
+            <br /> de software na DBServer.
           </p>
           <h2 className="sobreMim">Sobre mim</h2>
           <p className="textoSobreMim">
-            Cientista da Computação e Desenvolvedor JR<br />
-            ainda em formação. Conhecimento atual voltado<br />
-            para linguagens como Java, JS e ferramentas como<br />
-            HTML5, CSS3, Spring Boot, Swagger e Postman.<br />
-            Entusiasta da tecnologia, objetivo e criativo com<br />
-            o desejo de aprender. Pensamento voltado a entrega<br />
-            e a qualidade do código (Conceitos S.O.L.I.D, Clean Code).<br />
-            Em busca de uma oportunidade que proporcione<br />
-            crescimento profissional e aprimoramento<br />
-            como Desenvolvedor Full-Stack.
+            Um Cientista da Computação em formação e<br />
+            Desenvolvedor Fullstack Júnior, com conhecimentos
+            <br />
+            voltados para linguagens como TypeScript, JavaScript e Java
+            <br />
+            além de ferramentas e frameworks como HTML5, CSS3
+            <br />
+            NestJS, Node, Prettier, Eslint, Jest, Spring Boot, JUnit, Mockito
+            <br />
+            Docker, Swagger, Postman, Insomnia e Git.
+            <br />
+            <br />
+            Apaixonado por tecnologia, sou uma pessoa objetiva
+            <br />
+            criativa, com grande vontade de aprender e evoluir
+            <br />
+            constantemente na área de desenvolvimento. Tenho como
+            <br />
+            foco a entrega contínua e a qualidade do código, aplicando
+            <br />
+            Metodologias ágeis, princípios S.O.L.I.D e boas práticas de
+            <br />
+            Clean Code no meu dia a dia como programador.
           </p>
         </div>
 
         <div className="imagemHabilidades">
-          <img src="/images/fotoJoao.jpg" alt="João Victor" className="fotoJoao" />
+          <img
+            src="/images/fotoJoao.jpg"
+            alt="João Victor"
+            className="fotoJoao"
+          />
 
           <div className="habilidades">
             <h2 className="habilidadesTexto">Habilidades</h2>
             <p className="backEnd">Back end</p>
             <div className="backListaNivelDIV">
               <ul className="backEndLista">
-                <li>POSTMAN</li>
-                <li>JAVA</li>
-                <li>H2~SQL</li>
+                <li>NEST JS</li>
+                <li>TYPESCRIPT</li>
                 <li>SPRING BOOT</li>
-                <li>SWAGGER</li>
+                <li>JAVA</li>
+                <li>NODE</li>
+                <li>KNEX JS</li>
+                <li>SQL</li>
               </ul>
 
               <ul className="backListaNivel">
+                <li>
+                  <img src="/images/bolas3.png" alt="Nível" />
+                </li>
+                <li>
+                  <img src="/images/bolas3.png" alt="Nível" />
+                </li>
                 <li>
                   <img src="/images/bolas3.png" alt="Nível" />
                 </li>
@@ -67,7 +93,6 @@ const Home: React.FC = () => {
                 <li>CSS</li>
                 <li>JAVA SCRIPT</li>
                 <li>REACT</li>
-                <li>NODE</li>
               </ul>
 
               <ul className="frontListaNivel">
@@ -82,9 +107,6 @@ const Home: React.FC = () => {
                 </li>
                 <li>
                   <img src="/images/bolas3.png" alt="Nível" />
-                </li>
-                <li>
-                  <img src="/images/bolas1.png" alt="Nível" />
                 </li>
               </ul>
             </div>
